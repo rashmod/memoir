82fe8433025aece36d723ee999bc9cec69dce4bf
Bhushan <alhatbhushan@gmail.com>
2024-09-13 17:14:26 +0530

    [Frontend] feat: initialized frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div className="grid min-h-[100dvh] grid-rows-[auto_1fr]">
+      <div className=" container px-8    mx-auto">
+        <h1>hello world</h1>
+        <Button>click me</Button>
+      </div>
+    </div>
+  );
+}
