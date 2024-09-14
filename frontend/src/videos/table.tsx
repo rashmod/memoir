a90e414b121c5bbc5f36cd3cb6811351e3e96591
Bhushan <alhatbhushan@gmail.com>
2024-09-14 16:13:52 +0530

    [Frontend] feat: fetch video data using id

--- frontend/src/videos/table.tsx
+++ frontend/src/videos/table.tsx
@@ -1,10 +1,12 @@
+import { useEffect } from 'react';
+
 import history from '@/data/watch-history.json';
 
 import { DataTable } from '@/components/custom/data-table';
 import columns from '@/videos/columns';
 
 const data = (history as any[])
-  .slice(0, 1000)
+  .slice(0, 50)
   .filter((item) => !item.details)
   .filter((item) => item.titleUrl)
   .map((item) => ({
@@ -12,6 +14,27 @@ const data = (history as any[])
     url: item.titleUrl,
   }));
 
+const ids = data
+  .map((item) => {
+    const [, url] = item.titleUrl.split('=');
+    return 'id=' + url;
+  })
+  .join('&');
+console.log(ids);
+
 export default function Table() {
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=id&part=snippet&${ids}&key=AIzaSyDFofEQXnBEX5pmJA2B3_2RYa660-9YcNk`
+      );
+      const data = await response.json();
+
+      console.log(data);
+    }
+
+    getData();
+  }, []);
+
   return <DataTable data={data} columns={cols} />;
 }
