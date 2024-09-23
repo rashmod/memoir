4878146b8dcd477519b8cc6723a96bd367850f8f
Bhushan <alhatbhushan@gmail.com>
2024-09-23 23:35:03 +0530

    [Backend] feat: added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
-import env from './config/env';
+import env from '@/config/env';
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
