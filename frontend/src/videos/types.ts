8150546ccb2a988c3f46ebe8665b1196284e53e3
Bhushan <alhatbhushan@gmail.com>
2024-10-12 14:15:49 +0530

    [Frontend] chore: track video types file

---
+++ frontend/src/videos/types.ts
@@ -0,0 +1,44 @@
+import { z } from 'zod';
+
+export const importedVideoSchema = z.array(
+  z.object({
+    title: z.string(),
+    titleUrl: z.string().optional(),
+    time: z.string().datetime(),
+    subtitles: z
+      .array(
+        z.object({
+          name: z.string(),
+          url: z.string(),
+        })
+      )
+      .optional(),
+    details: z.any().optional(),
+  })
+);
+
+export type ImportedVideo = z.infer<typeof importedVideoSchema>;
+
+export type BasicVideo = {
+  youtubeId: string;
+  title: string;
+  url: string;
+  time: string;
+};
+
+export type DetailedVideo = BasicVideo & {
+  thumbnailUrl: string;
+  duration: number;
+  youtubeCreatedAt: Date;
+  channelId: string;
+  channelName: string;
+  channelUrl: string;
+  channelAvatarUrl: string;
+};
+
+export type MergedVideo = BasicVideo | DetailedVideo;
+
+export type FinalVideo = Omit<DetailedVideo, 'time'> & {
+  lastWatchedAt: string;
+  watchCount?: number;
+};
