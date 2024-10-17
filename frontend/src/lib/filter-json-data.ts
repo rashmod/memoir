b9caa7f00bdd52c66f4069a6c81f614cd720bee7
Bhushan <alhatbhushan@gmail.com>
2024-10-17 11:25:57 +0530

    [Frontend] refactor: updated zod schema for history json file

--- frontend/src/lib/filter-json-data.ts
+++ frontend/src/lib/filter-json-data.ts
@@ -2,18 +2,22 @@ import { ImportedVideo } from '@/videos/types';
 
 export default function filterJsonData(data: ImportedVideo) {
   return data
-    .filter((item) => !item.details)
-    .filter((item) => item.titleUrl)
+    .filter(hasNoDetails)
+    .filter((item) => 'titleUrl' in item)
     .map((item) => {
-      const subtitles = item.subtitles;
+      const subtitles = 'subtitles' in item && item.subtitles && item.subtitles.length > 0 ? item.subtitles : null;
 
-      const channelTitle = subtitles ? subtitles[0].name : undefined;
-      const channelUrl = subtitles ? subtitles[0].url : undefined;
+      const channelTitle = subtitles ? subtitles[0]!.name : undefined;
+      const channelUrl = subtitles ? subtitles[0]!.url : undefined;
       const title = item.title.replace('Watched ', '');
-      const url = item.titleUrl!;
+      const url = item.titleUrl;
       const [, youtubeId] = url.split('=');
       const time = item.time;
 
-      return { title, channelTitle, channelUrl, url, youtubeId, time };
+      return { title, channelTitle, channelUrl, url, time, youtubeId: youtubeId! };
     });
+}
+
+function hasNoDetails(item: ImportedVideo[number]): item is Exclude<typeof item, { details: any }> {
+  return !('details' in item);
 }
--- frontend/src/videos/types.ts
+++ frontend/src/videos/types.ts
@@ -1,21 +1,52 @@
 import { z } from 'zod';
 
-export const importedVideoSchema = z.array(
-  z.object({
-    title: z.string(),
-    titleUrl: z.string().optional(),
-    time: z.string().datetime(),
-    subtitles: z
-      .array(
-        z.object({
-          name: z.string(),
-          url: z.string(),
-        })
-      )
-      .optional(),
-    details: z.any().optional(),
-  })
-);
+/*
+ * ads will have details field
+ * some entries may not have subtitles field
+ * entries without subtitles field may be removed or made private
+ * shorts may not have subtitles field
+ * videos may not have subtitles
+ * surveys will not have titleUrl field
+ */
+
+const baseSchema = z.object({
+  title: z.string(),
+  time: z.string().datetime(),
+});
+
+// same for shorts
+const videoSchema = baseSchema.extend({
+  titleUrl: z.string(),
+  subtitles: z
+    .array(
+      z.object({
+        name: z.string(),
+        url: z.string(),
+      })
+    )
+    .optional(),
+});
+
+const removedVideoSchema = baseSchema.extend({
+  titleUrl: z.string(),
+});
+
+const questionnaireSchema = baseSchema.extend({
+  subtitles: z.array(
+    z.object({
+      name: z.string(),
+    })
+  ),
+});
+
+const adSchema = baseSchema.extend({
+  titleUrl: z.string(),
+  details: z.object({ name: z.string() }),
+});
+
+const schema = z.union([videoSchema, removedVideoSchema, adSchema, questionnaireSchema]);
+
+export const importedVideoSchema = z.array(schema);
 
 export type ImportedVideo = z.infer<typeof importedVideoSchema>;
 
